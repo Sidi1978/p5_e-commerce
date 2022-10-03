f8cb0a5f7384d77b09dc216bc0ac7abdc511b81b
A SIDI <ab.sidi1978@gmail.com>
2022-10-03 17:42:48 +0000

    affichage des produits sur index

---
+++ front/js/script.js
@@ -0,0 +1,35 @@
+
+// Récupération des produits de l'api
+ 
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((objetProduits) => {
+    console.table(objetProduits);
+
+    // appel  la fonction mesKanaps pour afficher des produits
+    mesKanaps(objetProduits);
+  })
+  // si une erreur : remplace le contenu de titre par  h1 au contenu de erreur 404 et renvoit en console l'erreur.
+  .catch((err) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404, sur ressource api:" + err);
+  });
+
+// fonction d'affichage des produits de l'api sur la page index
+
+function mesKanaps(index) {
+  // déclaration de variable de la zone d'article
+  let zoneArticle = document.querySelector("#items");
+  for (let article of index) {
+    /* création et ajout des zones d'articles, insertion dans l'adresse produit
+    la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
+
+    zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+    <article>
+      <img src="${article.imageUrl}" alt="${article.altTxt}">
+      <h3 class="productName">${article.name}</h3>
+      <p class="productDescription">${article.description}</p>
+    </article>
+  </a>`;
+  }
+}
